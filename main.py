981c7cb90b4bdcddd58669a8354c994b88b97964
Luis Chitala <53163045+luischitala@users.noreply.github.com>
2020-06-28 15:04:34 -0500

    Pipeline working

--- main.py
+++ main.py
@@ -1,19 +1,57 @@
 import argparse
 import logging
+
 logging.basicConfig(level=logging.INFO)
-
+import re
+from requests.exceptions import HTTPError
+from urllib3.exceptions import MaxRetryError
+import news_page_objects as news
 from common import config
-
 
 logger = logging.getLogger(__name__)
 
+is_well_formed_link = re.compile(r'^https?://.+/.+$') # https://example.com/hello
+is_root_path = re.compile(r'^/.+$') # /some-text
 
-def _news_scraper(news_site):
-    host = config()['news_sites'][news_site]['url']
+def _news_scraper(news_site_uid):
+    host = config()['news_sites'][news_site_uid]['url']
 
     logging.info('Beginning scraper for {}'.format(host))
-    logging.info('Finding links in homepage...')
+    homepage = news.HomePage(news_site_uid, host)
 
+    articles = []
+    for link in homepage.article_links:
+        article = _fetch_article(news_site_uid, host, link)
+
+        if article:
+            logger.info('Article fetched!')
+            articles.append(article)
+            print(article.title)
+
+    print(len(articles))
+
+def _fetch_article(news_site_uid, host, link):
+    logger.info('Start fetching article at {}'.format(link))
+
+    article = None
+    try:
+        article = news.ArticlePage(news_site_uid, _build_link(host, link))
+    except (HTTPError, MaxRetryError) as e:
+        logger.warning('Error while fetching the article', exc_info=False)
+
+    if article and not article.body:
+        logger.warning('Invalid article. There is no body')
+        return None
+    
+    return article
+
+def _build_link(host, link):
+    if is_well_formed_link.match(link):
+        return link
+    elif is_root_path.match(link):
+        return '{}{}'.format(host, link)
+    else:
+        return '{host}/{uri}'.format(host=host, uri=link)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -23,6 +61,8 @@ if __name__ == '__main__':
                         help='The news site that you want to scrape',
                         type=str,
                         choices=news_site_choices)
-
     args = parser.parse_args()
     _news_scraper(args.news_site)
+
+
+
